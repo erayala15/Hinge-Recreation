1f5bac045b65cec4dba7b21cf3f7f6598ca1c647
erayala15 <erayala@unc.edu>
2022-04-21 17:28:40 -0400

    finished dating profiles

---
+++ Hinge Recreation/PreferencesView.swift
@@ -0,0 +1,24 @@
+//
+//  PreferencesView.swift
+//  Hinge Recreation
+//
+//  Created by Ethan Rayala on 4/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        ScrollView {
+            Text("Sugma")
+        }
+        .navigationTitle("Dating Preferences")
+    }
+}
+
+struct PreferencesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
